a202a3956a0c4fb0b2c930575b060800659c04db
unknown <mstuhin>
2022-03-01 13:37:27 +0600

    searchfild

--- java.js
+++ java.js
@@ -1,4 +1,7 @@
 const searchButton = () => {
     const inputFild = document.getElementById("search-box").value;
-
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputFild}`;
+    fetch(url)
+        .then(res => res.json())
+        .then((data) =>)
 };
